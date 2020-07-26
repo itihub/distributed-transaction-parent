61d858ba7f544ef9bbd6d32bd261aa69ef35b341
jizhe <jizhe@live.cn>
2020-07-26 16:39:36 +0800

    添加建表语句

---
+++ xa-demo/src/main/resources/db/schema.sql
@@ -0,0 +1,15 @@
+
+
+-- 数据源 202 创建表
+CREATE TABLE `xa_202` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 数据源 203 创建表
+CREATE TABLE `xa_203` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
